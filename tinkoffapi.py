47403933120e42ec7a79dd6492e9e5ce538b410c
ElectronApps <barbwirek4@gmail.com>
2024-05-01 17:20:03 +0800

    Rewrite tinkoff api interface

--- tinkoffapi.py
+++ tinkoffapi.py
@@ -1,7 +1,8 @@
 import hashlib
+from typing import List
 import requests
-import json
 from dataclasses import dataclass
+from dataclasses_json import dataclass_json, LetterCase
 
 TINKOFF_URL = "https://securepay.tinkoff.ru"
 
@@ -18,55 +19,97 @@ def sign_data(data: dict, password: str) -> dict:
 
     return new_data
 
+
+@dataclass_json(letter_case=LetterCase.PASCAL)
 @dataclass
 class StandardPaymentRequest:
-    TerminalKey: str
-    Amount: int
-    OrderId: str
-    Description: str
-    SuccessURL: str
+    terminal_key: str
+    amount: int
+    order_id: str
+    description: str
+    successURL: str
+    failURL: str
 
+@dataclass_json(letter_case=LetterCase.PASCAL)
 @dataclass
 class StandardPaymentResponse:
-    Success: bool
-    ErrorCode: int
-    TerminalKey: str | None = None
-    Status: str | None = None
-    PaymentId: int | None = None
-    OrderId: str | None = None
-    Amount: int | None = None
-    PaymentURL: str | None = None
-    Message: str | None = None
-    Details: str | None = None
+    success: bool
+    error_code: int
+    terminal_key: str | None = None
+    status: str | None = None
+    payment_id: int | None = None
+    order_id: str | None = None
+    amount: int | None = None
+    paymentURL: str | None = None
+    message: str | None = None
+    details: str | None = None
 
-def init_standard(terminal_key: str, password: str, amount: int, order_id: str) -> StandardPaymentResponse:
-    request = StandardPaymentRequest(terminal_key, amount, order_id, "тестовое описание", "https://google.com")
-    signed_data = sign_data(vars(request), password)
+def init_standard(terminal_key: str, password: str, amount: int, order_id: str, description: str, success_url: str, fail_url: str) -> StandardPaymentResponse:
+    request = StandardPaymentRequest(terminal_key, amount, order_id, description, success_url, fail_url)
+    signed_data = sign_data(request.to_dict(), password)
     
     response = requests.post(url=f"{TINKOFF_URL}/v2/Init", json=signed_data)
-    return StandardPaymentResponse(**response.json())
+    return StandardPaymentResponse.from_dict(response.json())
 
+
+@dataclass_json(letter_case=LetterCase.PASCAL)
 @dataclass
 class CheckPaymentRequest:
-    TerminalKey: str
-    PaymentId: int
+    terminal_key: str
+    payment_id: int
 
+@dataclass_json(letter_case=LetterCase.PASCAL)
 @dataclass
 class CheckPaymentResponse:
-    Success: bool
-    ErrorCode: int
-    TerminalKey: str | None = None
-    Status: str | None = None
-    PaymentId: int | None = None
-    OrderId: str | None = None
-    Amount: int | None = None
-    Message: str | None = None
-    Details: str | None = None
-    Params: any = None
+    success: bool
+    error_code: int
+    terminal_key: str | None = None
+    order_id: str | None = None
+    message: str | None = None
+    details: str | None = None
+    params: any = None
 
-def check_payment(terminal_key: str, password: str, payment_id: int):
+def check_payment(terminal_key: str, password: str, payment_id: int) -> CheckPaymentResponse:
     request = CheckPaymentRequest(terminal_key, payment_id)
-    signed_data = sign_data(vars(request), password)
+    signed_data = sign_data(request.to_dict(), password)
     
     response = requests.post(url=f"{TINKOFF_URL}/v2/GetState", json=signed_data)
-    return CheckPaymentResponse(**response.json())
+    return CheckPaymentResponse.from_dict(response.json())
+
+
+@dataclass_json(letter_case=LetterCase.PASCAL)
+@dataclass
+class CheckOrderRequest:
+    terminal_key: str
+    order_id: int
+
+@dataclass_json(letter_case=LetterCase.PASCAL)
+@dataclass
+class CheckOrderPaymentsResponse:
+    success: bool
+    payment_id: str
+    amount: int
+    status: str
+    error_code: int | None = None
+    RRN: str | None = None
+    message: str | None = None
+
+@dataclass_json(letter_case=LetterCase.PASCAL)
+@dataclass
+class CheckOrderResponse:
+    success: bool
+    error_code: int
+    terminal_key: str | None = None
+    order_id: str | None = None
+    message: str | None = None
+    details: str | None = None
+    payments: List[CheckOrderPaymentsResponse] | None = None
+
+def check_order(terminal_key: str, password: str, order_id: int) -> CheckOrderResponse:
+    request = CheckOrderRequest(terminal_key, order_id)
+    signed_data = sign_data(request.to_dict(), password)
+    
+    response = requests.post(url=f"{TINKOFF_URL}/v2/CheckOrder", json=signed_data)
+    return CheckOrderResponse.from_dict(response.json())
+
+print(check_order("TinkoffBankTest", "TinkoffBankTest", "1337"))
